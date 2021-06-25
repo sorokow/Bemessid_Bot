4ad826e291913510e548e563288294472737c74f
sorokow <sorokov_a02@mail.ru>
2021-06-25 20:43:10 +0300

    Frame created (without functionality)

---
+++ src/main/java/com/example/demessid_bot/Config/BotConfig.java
@@ -0,0 +1,30 @@
+package com.example.demessid_bot.Config;
+
+import com.example.demessid_bot.Bot;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    @Bean
+    public Bot mySuperBot(){
+        Bot bot = new Bot();
+        bot.setWebHookPath(webHookPath);
+        bot.setBotToken(botToken);
+        bot.setBotUserName(botUserName);
+        return bot;
+
+    }
+}
---
+++ src/main/java/com/example/demessid_bot/DemessidBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demessid_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemessidBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemessidBotApplication.class, args);
+    }
+
+}
